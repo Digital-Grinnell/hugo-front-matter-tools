0c65614f1e5454c79be302245247573cd8aa293d
Mark McFate <summitt.dweller@gmail.com>
2022-11-19 15:10:25 -0600

    Sheet to front matter script is complete except for lists

--- rootstalk-google-sheet-to-front-matter.py
+++ rootstalk-google-sheet-to-front-matter.py
@@ -15,7 +15,6 @@ from datetime import datetime
 from queue import Empty
 from typing import Dict
 import frontmatter
-import csv
 import gspread as gs
 
 required_fields = {
@@ -36,109 +35,24 @@ editable_fields = {
   "no_leaf_bug": "no_leaf_bug"
 }
 
-# def truncate(text):
-#   if type(text) is str:
-#     if len(text) > 132:
-#       return text[:129] + "..."
-#   return text  
+def process_record(rec, path):
+  try:
+    post = frontmatter.load(path)
+  except Exception as e:
+    print(e)
 
-# def process_contributors(contributors, contributor_fields):
-#   c_filtered = dict()
-#   for f in contributor_fields:
-#     if f in contributors[0].keys():
-#       c_filtered[f] = contributors[0][f]
-#     else: 
-#       c_filtered[f] = ""
-#   return len(contributors), c_filtered 
+  for key,value in rec.items():
+    if key in editable_fields:
+      post[key] = value
 
-# def build_link(k, path):
-#   base_urls = { "develop":"https://thankful-flower-0a2308810.1.azurestaticapps.net/", "main":"https://icy-tree-020380010.azurestaticapps.net/", "production":"https://rootstalk.grinnell.edu/" }
-#   filename = path.name
-#   parent = path.parent.name
-#   grandma = path.parent.parent.name
-#   if (filename == "_index.md"):
-#     filename = ""
-#   if "past" in grandma:
-#     url = f"{base_urls[k]}{grandma}/{parent}/{filename}".rstrip(" .md")
-#   else:
-#     url = f"{base_urls[k]}{parent}/{filename}".rstrip(" .md")
-#   return f"{url} "   # blank at the end is necessary for links to work properly
-
-# def parent_path(path):
-#   parent = path.parent.name
-#   grandma = path.parent.parent.name
-#   if "past" in grandma:
-#     return f"{grandma}/{parent}"
-#   else:
-#     return f"{parent}"
-
-# # Lifted from https://stackoverflow.com/a/54231563
-# #  csv_file - path to csv file to upload
-# #  sheet - a gspread.Spreadsheet object
-# #  cell - string giving starting cell, optionally including sheet/tab name
-# #    example: 'A1', 'MySheet!C3', etc.
-# def paste_csv(csv_file, sheet, cell):
-#   if '!' in cell:
-#     (tab_name, cell) = cell.split('!')
-#     wks = sheet.worksheet(tab_name)
-#   else:
-#     wks = sheet.sheet1
-#   (first_row, first_column) = gs.utils.a1_to_rowcol(cell)
-
-#   with open(csv_file, 'r') as f:
-#     csv_contents = f.read()
-#   body = {
-#     'requests': [{
-#       'pasteData': {
-#         "coordinate": {
-#           "sheetId": wks.id,
-#           "rowIndex": first_row-1,
-#           "columnIndex": first_column-1,
-#         },
-#         "data": csv_contents,
-#         "type": 'PASTE_NORMAL',
-#         "delimiter": ',',
-#       }
-#     }]
-#   }
-#   return sheet.batch_update(body)
-    
-# # From an example at https://pypi.org/project/gspread-formatting/
-# def format_google_sheet(sheet, tab_name):
-#   bold = gsf.cellFormat(
-#     backgroundColor=gsf.color(0.9, 0.9, 0.9),
-#     textFormat=gsf.textFormat(bold=True, foregroundColor=gsf.color(0, 0, 0)),
-#     )
-#   wks = sheet.worksheet(tab_name)
-#   batch = gsf.batch_updater(sheet)
-#   batch.set_frozen(wks, rows=1)
-#   batch.format_cell_ranges(wks, [('A1:Z1', bold)])
-#   return batch.execute()
-
-
-# # From an example at https://pypi.org/project/gspread-formatting/
-# def highlight_todo_cells(sheet, tab_name):
-#   wks = sheet.worksheet(tab_name)
-#   rule = gsf.ConditionalFormatRule(
-#     ranges=[gsf.GridRange.from_a1_range('H2:H2000', wks)],
-#     booleanRule=gsf.BooleanRule(
-#         condition=gsf.BooleanCondition('NOT_BLANK'),
-#         format=gsf.cellFormat(textFormat=gsf.textFormat(bold=True), backgroundColor=gsf.Color(1,1,0))
-#     )
-#   )
-
-#   rules = gsf.get_conditional_format_rules(wks)
-#   rules.append(rule)
-#   rules.save()
-
-  # # Read the 'Update' sheet into a Pandas dataframe for processing.  
-  # # Per https://medium.com/geekculture/2-easy-ways-to-read-google-sheets-data-using-python-9e7ef366c775
-  # SHEET_ID = '1cOYyS5gwU3HbTG8aVkaBwFPL1Z_7U25bJBCKCePFafI'
-  # SHEET_NAME = 'Update'
-  # url = f'https://docs.google.com/spreadsheets/d/{SHEET_ID}/gviz/tq?tqx=out:csv&sheet={SHEET_NAME}'
-  # df = pd.read_csv(url, sep='\t')
-  # print(df.dtypes)    # print the header row
-
+  # now dump the frontmatter post back into the file
+  try:
+    md = open(path, "w")
+    md.write(frontmatter.dumps(post))
+    md.close
+  except Exception as e:
+    print(e)
+  return
 
 ######################################################################
 
@@ -163,12 +77,6 @@ if __name__ == '__main__':
     # Found the "Update" sheet
     if (title == "Update"):
 
-      # Determine if the required_fields are present
-      for req in required_fields.values():
-        if req not in headers:
-          msg = print(f"Error: Required header '{req}' was NOT found in the 'Update' worksheet!")
-          sys.exit(msg)
-
       # Generate a temporary .csv of the worksheet 
       # per https://community.esri.com/t5/python-questions/how-to-convert-a-google-spreadsheet-to-a-csv-file/td-p/452722
       with open('temp.csv', 'w', newline='') as csvfile:
@@ -178,140 +86,12 @@ if __name__ == '__main__':
       # Read the temporary .csv into a dict
       with open('temp.csv', 'r') as data:
         for record in csv.DictReader(data):
+
           # worksheet record now in dict form, process it for updates
+          md_path = str(pathlib.Path.home()) + "/GitHub/rootstalk/content/" + record['Content Path'] + "/" + record['Filename'] + ".md"
+          if os.path.exists(md_path):
+            process_record(record, md_path)
+          else:
+            print(f"Warning: File '{md_path}' does not exist so it's record has been skipped.")
 
-
-
-
-
-
-
-      # # Determine if any editable_fields are present
-      # found = False
-      # found_headers = []
-      
-      # for edt in editable_fields.values():
-      #   if edt in headers:
-      #     found = True
-      #     found_headers.append(edt)
-
-      # # If any editable_fields were found, loop row-by-row
-      # if found:
-      #   for row in range(2,nrows):
-      #     values = ws.row_values(row)      
-
-
-  # csv_filename = "front-matter-status.csv"
-  
-  # # Open the .csv file for output
-  # with open(csv_filename, "w") as csvfile:
-  #   writer = csv.DictWriter(csvfile, fieldnames=fields.values())
-  #   writer.writeheader()
-
-  #   # Specify the path to be processed...
-  #   filepath = str(pathlib.Path.home()) + "/GitHub/rootstalk/content/**/volume*/*.md"
-  
-  #   # Iterate over the working directory tree + subdirectories for all {issue}/{article}.md files
-  #   # Using '*.md' pattern recursively
-  #   for file in glob.glob(filepath, recursive=True):
-    
-  #     path = pathlib.PurePath(file)
-  #     article = frontmatter.load(file)
-  #     obsolete = []
-  #     filtered = dict()  # must be sure to initialize this to empty here!
-
-  #     # Loop on each top-level element of the article's front matter 
-  #     for key in article.metadata:
-
-  #       # Found a key that we didn't expect... warning
-  #       if key not in fields.keys():
-  #         assert key not in obsolete, "Error: Front matter key '{key}' does not exist in ANY list!"
-
-  #         print(f"Warning: Front matter key '{key}' is obselete. This article needs to be updated!")
-  #         obsolete.append(key)
-        
-  #       # We have an expected top-level key and value
-  #       else:
-  #         value = article.metadata[key]
-
-  #         # If we have a list...
-  #         if type(value) is list:
-  #           if key == "contributors":
-  #             c = value[0]
-  #             for f in contributor_fields:
-  #               if f in c.keys():
-  #                 filtered[fields[f]] = truncate(c[f])
-  #               else:
-  #                 filtered[fields[f]] = ""
-  #             value = len(value)
-
-  #           # Just a list, nothing special  
-  #           else:  
-  #             value = ",".join(value)
-
-  #         # If we have a dict...
-  #         if type(value) is dict:
-  #           if key == "header_image":
-  #             for f in header_image_fields:
-  #               if f in value.keys():
-  #                 filtered[fields[f]] = truncate(value[f])
-  #               else:
-  #                 filtered[fields[f]] = ""
-  #             value = True
-  #           else:
-  #             print(f"Warning: Unexpected front matter dict {key} found!")
-
-  #         filtered[fields[key]] = truncate(value)
-    
-  #     # Seed the .csv row with path and filename
-  #     filtered[fields['md-file']] = path.name[:-3]
-  #     filtered[fields['md-path']] = parent_path(path)
-
-  #     # Build one live link for each code branch and seed the .csv row with them
-  #     for key in branches:
-  #       key_name = f"{key}-link"
-  #       filtered[fields[key_name]] = build_link(key, path)
-
-  #     # Note any obsolete front matter
-  #     filtered[fields['obsolete']] = obsolete
-
-  #     writer.writerow(filtered)
-
-  # # Ok, done writing the .csv, now copy it to a new tab/worksheet in our Google Sheet
-  # # Open the Google service account and sheet
-  # try:
-  #   sa = gs.service_account()
-  # except Exception as e:
-  #   print(e)
-
-  # try:  
-  #   sh = sa.open("Rootstalk Articles Front Matter")
-  # except Exception as e:
-  #   print(e)  
-
-  # # Make a datestamp to name the new worksheet
-  # sheet_name = datetime.now().strftime("%Y-%b-%d-%I:%M%p")
-  
-  # # Create the new/empty worksheet
-  # try:
-  #   worksheet = sh.add_worksheet(title=sheet_name, rows=1, cols=1)
-  # except Exception as e:
-  #   print(e)  
-
-  # # Call our function to write the new Google Sheet worksheet
-  # try:
-  #   paste_csv(csv_filename, sh, sheet_name + "!A1")
-  # except Exception as e:
-  #   print(e)
-
-  # # Call our format function to set the overall format of the new sheet
-  # try:
-  #   format_google_sheet(sh, sheet_name)
-  # except Exception as e:
-  #   print(e)
-
-  # # Call our function to set conditional formatting rules
-  # try:
-  #   highlight_todo_cells(sh, sheet_name)
-  # except Exception as e:
-  #   print(e)
+  exit()
